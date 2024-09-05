a3b55de8a2d2389dd738058122c18f09c8725ca7
stLmpp <gui.stlmpp@gmail.com>
2024-09-05 15:44:22 -0300

    fix: export type

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export { InjectionToken } from './injection-token.js';
 export { Injector } from './injector/injector.js';
 export { isProvider, type Provider, type Provide } from './provider/provider.js';
 export { ROOT_INJECTOR, RootInjector } from './injector/root-injector.js';
-export { UnwrapProviders } from './type.js';
+export type { UnwrapProviders } from './type.js';
 export { ClassProvider } from './provider/class-provider.js';
 export { FactoryProvider } from './provider/factory-provider.js';
 export { ValueProvider } from './provider/value-provider.js';
